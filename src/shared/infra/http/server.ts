1e903752af3265b28d0a457fc1e07e78011e8e76
Gabriel Brogni <gabrielbrogni@hotmail.com>
2021-01-24 16:57:23 -0300

    added cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
+import cors from 'cors'
 import { errors } from 'celebrate'
 
 import routes from './routes/routes'
@@ -9,6 +10,7 @@ import AppError from '@shared/errors/AppError'
 
 const app = express()
 
+app.use(cors({}))
 app.use(express.json())
 app.use(routes)
 app.use(errors())
