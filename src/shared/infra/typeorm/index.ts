dc2809c2a6085521355e0d03c3a2dd06406eb59f
Gabriel Brogni <gabrielbrogni@hotmail.com>
2021-02-07 19:09:48 -0300

    deploy

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,9 @@
 import { createConnections } from 'typeorm'
 
 createConnections()
+  .then(success => {
+    console.log('Connection established with success:' + success)
+  })
+  .catch(error => {
+    console.error('Error establishing the connection:' + error)
+  })
